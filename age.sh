b7ca5861d51fa0a0ff314fb96e2faacd3c0f2611
abc <abc@gmail.com>
2024-10-28 11:13:55 -0400

    added first

--- age.sh
+++ age.sh
@@ -5,8 +5,10 @@ if [ $x -ge 0 -a $x -lt  13 ];then
 echo "Child"
 elif [ $x -ge 13 -a $x -le 19 ];then
 echo "Teenager"
-elif [ $x -ge 20 ];then
+elif [ $x -ge 20 -a $x -le 60 ];then
 echo "Adult"
+elif [ $x -ge 61 ];then
+echo "Old Age"
 else
 echo "Pls Enter Age in Positive"
 fi
--- tax.sh
+++ tax.sh
@@ -3,6 +3,8 @@ echo "Enter Your Salary"
 read x
 if [ $x -ge 0 -a  $x -lt 20000 ];then
 echo "No Tax"
+elif [ $x -ge 20000 ];then
+echo "2% tax"
 elif [ $x -gt 20000 -a  $x -le 40000 ];then
 echo "5% tax"
 elif [ $x -gt 40000 ];then
